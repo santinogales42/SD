d9eeae80fab117d119a5dca1ac441faf02213905
AlejandroMolines <117664615+AlejandroMolines@users.noreply.github.com>
2023-12-16 11:47:28 +0100

    Token, autenticacion, iniciar sesion...

--- Practica3/Prac3/API_Rest.py
+++ Practica3/Prac3/API_Rest.py
@@ -3,6 +3,8 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from pymongo import MongoClient, errors
 from bson import ObjectId
 from datetime import timedelta
+from cryptography.fernet import Fernet
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
 import logging
 
@@ -19,19 +21,33 @@ def home():
 
 
 ###### SEGURIDAD ######
+
+@app.route('/registro_usuario', methods=['POST'])
+def registro_usuario():
+    username = request.json.get("username")
+    password = request.json.get("password")
+    password_hash = generate_password_hash(password)
+
+    if db.users.find_one({"username": username}):
+        return jsonify({"msg": "El usuario ya existe"}), 409
+
+    db.users.insert_one({"username": username, "password_hash": password_hash})
+    return jsonify({"msg": "Usuario registrado exitosamente"}), 201
+
+
+
+
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
-    
-    # Aquí debes validar las credenciales del dron
-    if username != "dron_test" or password != "password_test":
-        return jsonify({"msg": "Credenciales incorrectas"}), 401
+    username = request.json.get("username")
+    password = request.json.get("password")
+    user = db.users.find_one({"username": username})
 
-    expires = timedelta(seconds=20)
-    access_token = create_access_token(identity=username, expires_delta=expires)
+    if user and check_password_hash(user['password_hash'], password):
+        access_token = create_access_token(identity=username, expires_delta=timedelta(seconds=20))
+        return jsonify(access_token=access_token)
+    return jsonify({"msg": "Credenciales incorrectas"}), 401
 
-    return jsonify(access_token=access_token)
 
 @app.route('/protegido', methods=['GET'])
 @jwt_required()
@@ -43,6 +59,7 @@ def protegido():
 #logging.basicConfig(filename='registro_auditoria.log', level=logging.INFO)
 
 #@app.route('/evento', methods=['GET'])
+#@jwt_required()
 #def evento():
     #logging.info("Evento registrado")
     #return jsonify(msg="Evento registrado")
@@ -50,11 +67,14 @@ def protegido():
 
 
 ##### API #####
+@app.errorhandler(Exception)
+def error_response(e):
+    logging.error(f'Error: {e}')
+    return jsonify(error=str(e)), 500
 
-def error_response(message, status_code):
-    return jsonify({'error': message}), status_code
 
 @app.route('/registro', methods=['POST'])
+@jwt_required()
 def registro():
     try:
         print(request.data)  # Ver los datos brutos que llegan
@@ -63,7 +83,7 @@ def registro():
         drone_id = str(data.get('ID', '')).strip()
         alias = data.get('Alias', '').strip()
         initial_position = [1, 1]  # Posición inicial predeterminada
-
+        
         if not alias or not drone_id:
             return jsonify({'error': 'Falta el alias o el ID del dron'}), 400
 
@@ -77,12 +97,14 @@ def registro():
 
 
 @app.route('/listar_drones', methods=['GET'])
+@jwt_required()
 def listar_drones():
     try:
-        drones = list(db.drones.find({}, {'ID': 1, 'Alias': 1}))
-        return jsonify([{'ID': str(drone['ID']), 'Alias': drone['Alias']} for drone in drones])
+        drones = list(db.drones.find({}, {'_id': 1, 'alias': 1}))
+        return jsonify([{'ID': str(drone['_id']), 'Alias': drone.get('alias', 'Sin Alias')} for drone in drones])
     except errors.PyMongoError as e:
         return error_response(f'Error de base de datos: {e}', 500)
+
 
 @app.route('/modificar_dron/<drone_id>', methods=['PUT'])
 def modificar_dron(drone_id):
@@ -102,12 +124,14 @@ def modificar_dron(drone_id):
 @app.route('/borrar_dron/<drone_id>', methods=['DELETE'])
 def borrar_dron(drone_id):
     try:
-        result = db.drones.delete_one({'_id': ObjectId(drone_id)})
+        drone_id = int(drone_id) if drone_id.isdigit() else ObjectId(drone_id)
+        result = db.drones.delete_one({'_id': drone_id})
         if result.deleted_count == 0:
-            return error_response('Dron no encontrado', 404)
+            return jsonify({'error': 'Dron no encontrado'}), 404
         return jsonify({'message': 'Dron eliminado correctamente'}), 200
     except errors.PyMongoError as e:
-        return error_response(f'Error de base de datos: {e}', 500)
+        return jsonify({'error': str(e)}), 500
+
 
 @app.route('/reiniciar_registro', methods=['DELETE'])
 def reiniciar_registro():
